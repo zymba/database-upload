76def2016581e1eee91aed1d41ba083039ef1d4b
Bueno <zymbabueno@gmail.com>
2020-04-27 19:03:17 -0300

    Delete transaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,19 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const repository = await transactionsRepository.findOne(id);
+
+    if (!repository) {
+      throw new AppError('Transação não encontrada');
+    }
+
+    await transactionsRepository.remove(repository);
   }
 }
 
